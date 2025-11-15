2a9d7701a7c34a5e368d7b6220413a641ba2d532
Satyaprakash <satyaasingh001@gmail.com>
2025-11-15 15:12:44 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -89,13 +89,13 @@ app.delete('/tasks/:id', async (req, res) => {
 });
 
 // PRODUCTION DEPLOYMENT
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+//   });
+// }
 
 
 const PORT = process.env.PORT || 5000;
